aacc180a297ff61f326aa2afc8cfe03475a26d31
Lucas <lucurban@gmail.com>
2025-07-16 22:07:05 -0500

    Confirmando cambios 250716

--- ManejoInventarioVentas.py
+++ ManejoInventarioVentas.py
@@ -1,2 +1,20 @@
+'''
+Este programa hace registro de ventas y modifica el inventario de una tienda
+'''
+# --- Importar paquetes
 import pandas as pd
-import numpy as np
+
+inventario = 'Inventario.xlsx'
+
+df_inventario = pd.read_excel(inventario)
+
+print(df_inventario)
+
+print(df_inventario['Producto'])
+'''
+producto = input('producto: ')
+
+verificar = df_inventario['Cantidad'] = producto
+
+print(verificar)
+'''
