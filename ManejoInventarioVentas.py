9a7b0c8bccf8817d848d6eac11acfb5e5747e5bc
Lucas <lucurban@gmail.com>
2025-07-20 18:39:24 -0500

    250720

--- ManejoInventarioVentas.py
+++ ManejoInventarioVentas.py
@@ -10,8 +10,19 @@ df_inventario = pd.read_excel(inventario)
 
 print(df_inventario)
 
-print(df_inventario['Producto'])
+
+print(df_inventario['Cantidad'] = )
+
 '''
+df_add_inventario = pd.DataFrame([['Loción California', 16, 'und']],
+                     index=[7],
+                     columns=['Producto', 'Cantidad', 'Unidad'])
+
+with pd.ExcelWriter(inventario, mode='a') as writer:
+    df_add_inventario.to_excel(writer, 'sheet2')
+    
+print(df_inventario)
+
 producto = input('producto: ')
 
 verificar = df_inventario['Cantidad'] = producto
